54a35cfa3a5fafb9b9e9b7b16b0afbc421e99ff5
Ben McRae <benmcrae5@yahoo.com>
2021-01-10 12:54:35 -0700

    progress made...

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,10 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize({
+      database: process.env.DB_NAME, 
+      username: process.env.DB_USER, 
+      password: process.env.DB_PW, 
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,24 +7,51 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   const categories = await Category.findAll();
-  res.send(categories);
+  res.json(categories);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  const { id } = req.params;
+  const category = await Category.findAll({
+    where: {
+      id,
+    }
+  })
+  res.json(category)
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  const { category_name } = req.body;
+  const category = Category.create({ category_name });
+  res.json(category);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  const { id, category_name } = req.body;
+  const category = await Category.update(
+    {
+      category_name,
+    },
+    {
+      where: { id },
+    }
+  );
+  res.json(category);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  const { id } = req.params;
+  const category = await Category.destroy({
+    where: {
+      id,
+    }
+  })
+  res.json(category);
 });
 
 module.exports = router;
